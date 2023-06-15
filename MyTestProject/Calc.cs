1076d4341c2cb1234ffb3b4c9a691e72a824950f
dvndmy <74776505+dvndmy@users.noreply.github.com>
2023-06-15 15:51:29 +0100

    Calc tests implemented

---
+++ MyTestProject/Calc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTestProject
+{
+    internal class Calc
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ MyTestProject/CalcTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MyTestProject
+{
+    [TestFixture]
+    class CalcTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            // Create an instance of the Calc class
+            Calc c = new Calc();
+
+            // Set the values of x and y
+            double x = 4.0;
+            double y = 5.0;
+
+            // Set the expected value of the addition
+            double expectedValue = 5.0;
+
+            // Perform the addition and get the actual value
+            double actualValue = c.Add(x, y);
+
+            // Check if the actual value matches the expected value
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+        [Test]
+        public void SubTest()
+        {
+            // Create an instance of the Calc class
+            Calc c = new Calc();
+
+            // Set the values of x and y
+            double x = 9.0;
+            double y = 5.0;
+
+            // Set the expected value of the addition
+            double expectedValue = 2.0;
+
+            // Perform the addition and get the actual value
+            double actualValue = c.Sub(x, y);
+
+            // Check if the actual value matches the expected value
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+
+
+}
